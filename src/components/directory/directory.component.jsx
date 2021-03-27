62fa67ff92ecd2dd8714941f83db58a5e6744993
PlengNakdee <plengwn@gmail.com>
2021-03-27 09:05:26 +0000

    checkpoint

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './directory.styles.scss';
+import MenuItem from '../menu-item/menu-item.component';
 
 class Directory extends React.Component {
     constructor() {
         super();
         this.state = {
-            section: [
+            sections: [
                 {
                     title: 'hats',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
@@ -41,4 +42,17 @@ class Directory extends React.Component {
             ]
         }
     }
+     render() {
+            return (
+                <div className='directory-menu'>
+                    {
+                        this.state.sections.map(({title, imageUrl, id, size}) => (
+                            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                        ))
+                    }
+                </div>
+            )
+        }
 }
+
+export default Directory;
